e30a3c1e37bc237b102a6058d427fd41691876fa
hjlim <teddycns3@gmail.com>
2022-07-11 18:23:16 +0900

    css 모듈화 적용 전

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import './app.css';
+import Button1 from './component/button1';
+import Button2 from './component/button2';
+
+function app() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+    </>
+  );
+
+}
+
+export default app;
---
+++ src/component/button1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './button1.css'
+
+class Button1 extends Component {
+    render() {
+        return (
+            <div className='button1_button'>
+                <span className='button1_text'>Button1</span>
+            </div>
+        );
+    }
+}
+
+export default Button1;
