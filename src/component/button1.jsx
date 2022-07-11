ea1d424c02e7d6a5ae75e2a5088ec2c60fd85449
hjlim <teddycns3@gmail.com>
2022-07-11 18:26:11 +0900

    css 모듈화

--- src/component/button1.jsx
+++ src/component/button1.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './button1.css'
+import styles from './button1.module.css'
 
 class Button1 extends Component {
     render() {
         return (
-            <div className='button1_button'>
-                <span className='button1_text'>Button1</span>
+            <div className={styles.button}>
+                <span className={styles.text}>Button1</span>
             </div>
         );
     }
--- src/component/button2.jsx
+++ src/component/button2.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import  './button2.css'
+import styles from './button2.module.css'
 
 class Button2 extends Component {
     render() {
         return (
-            <div className="button2_button">
-                <span className="button2_text">Button2</span>
+            <div className={styles.button}>
+                <span className={styles.text}>Button2</span>
             </div>
         );
     }
